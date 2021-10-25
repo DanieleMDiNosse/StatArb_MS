8fc25747b81744c5e187c2d54640b205b9cbbab1
DanieleMDiNosse <danielemdinosse@gmail.com>
2021-10-25 17:31:07 +0200

    checkpoint

--- statarb_ms/scoring.py
+++ statarb_ms/scoring.py
@@ -2,8 +2,9 @@ import numpy as np
 import logging
 import argparse
 import pandas as pd
-from factors import *
-from regression_parameters import *
+from makedir import go_up
+from factors import pca, risk_factors, money_on_stock
+from regression_parameters import regression
 import time
 import multiprocessing as mp
 import os
@@ -39,7 +40,7 @@ def generate_score(df_returns, n_factor, variable_n_factor=False, lookback_for_f
 
     trading_days = df_returns.shape[0] - lookback_for_factors
     n_stocks = df_returns.shape[1]
-    df_score = df_returns[:trading_days].copy(deep=True) # Il primo score corrisponderà al 252° giorno (indice 251)
+    score = np.zeros(shape=(trading_days, n_stocks)) # Il primo score corrisponderà al 252° giorno (indice 251)
     beta_tensor = np.zeros(shape=(trading_days, n_stocks, n_factor))
     risk_fact = np.zeros(shape=(trading_days, n_factor))
     Q = np.zeros(shape=(trading_days, n_factor, n_stocks))
@@ -48,17 +49,18 @@ def generate_score(df_returns, n_factor, variable_n_factor=False, lookback_for_f
         # Una finestra temporale di 252 giorni è traslata di 1 giorno. Ogni volta viene eseguita una PCA su tale periodo
         # ed i fattori di rischio sono quindi valutati.
         logging.info(f'Trading day: {lookback_for_factors+i}')
-        period = df_returns[i:lookback_for_factors + i]
+        period = df_returns[i:lookback_for_factors + i] # [0,252[, [1,253[ ecc -> ogni period comprende un anno di trading (252 giorni)
         if variable_n_factor:
             eigenvalues, eigenvectors = pca(
                 period, n_components=n_factor, variable_number=variable_n_factor)
         else:
             eigenvalues, eigenvectors = pca(
                 period, n_components=n_factor, variable_number=variable_n_factor)
-        factors = risk_factors(period, eigenvectors) # ritorni dei fattori di rischio
+        eigenvectors = -eigenvectors
+        factors = risk_factors(period, eigenvectors) # ritorni dei fattori di rischio per ogni periodo
 
         risk_fact[i,:] = factors[-1] # mi serve sostanzialmente solamente l'ultimo vettore dei ritorni dei fattori per il trading
-        Q[i,:,:] = money_on_stock(period, eigenvectors) # ogni giorno so quanto investire du ogni compagnia all'interno di ognuno dei fattori
+        Q[i,:,:] = money_on_stock(period, eigenvectors) # trading_days x n_factors x n_stocks. Ogni giorno so quanto investire su ogni compagnia all'interno di ognuno dei fattori
 
         # Ogni giorno i ritorni dei fattori sono valutati su finestre temporali di lunghezza 252 giorni ma che differiscono di un giorno.
         # Devo tener traccia di tutti i ritorni dei fattori di rischio giorno per giorno poiché mi serviranno per il trading.
@@ -67,15 +69,14 @@ def generate_score(df_returns, n_factor, variable_n_factor=False, lookback_for_f
 
         # Ottenuti i fattori di rischio si procede con la stima del processo dei residui per ogni compagnia.
         for stock in df_returns.columns:
-            # Porre l'intercetta nulla corrisponde a porre il drift alpha nullo
-            predictions, beta0, betas, residuals = regression(factors[-lookback_for_residual:], period[-lookback_for_residual:][stock], fit_intercept=False)
-            beta_tensor[i, df_returns.columns.get_loc(stock), :] = betas
+            stock_idx = df_returns.columns.get_loc(stock)
+            predictions, beta0, betas, residuals = regression(factors[-lookback_for_residual:], period[-lookback_for_residual:][stock], fit_intercept=True)
+            beta_tensor[i, stock_idx, :] = betas
 
             discreteOU = np.zeros(len(residuals[-lookback_for_residual:]))
             for j in range(len(discreteOU)):
                 discreteOU[j] = residuals[:j + 1].sum()
-            discreteOU_predicted, a, b, discrete_residuals = regression(
-                discreteOU[:-1].reshape(-1, 1), discreteOU[1:], fit_intercept=True)
+            discreteOU_predicted, a, b, discrete_residuals = regression(discreteOU[:-1].reshape(-1, 1), discreteOU[1:], fit_intercept=True)
 
             if b == 0.0:
                 print(f'B NULLO PER {stock}')
@@ -83,15 +84,16 @@ def generate_score(df_returns, n_factor, variable_n_factor=False, lookback_for_f
 
             k = -np.log(b) * lookback_for_factors
             if k < lookback_for_factors / (0.5 * lookback_for_residual):
-                df_score[stock][i] = 0
+                score[i, stock_idx] = 0
             else:
                 m = a / (1 - b)
                 sgm = np.std(discrete_residuals) * np.sqrt(2 * k / (1 - b * b))
                 sgm_eq = np.std(discrete_residuals) * np.sqrt(1 / (1 - b * b))
                 # naive method. Keep in mind that s-score depends on the risk factors
                 s_score = -m / sgm_eq
-                df_score[stock][i] = s_score[0]
+                score[i, stock_idx] = s_score[0]
 
+    df_score = pd.DataFrame(score, columns=df_returns.columns)
     if export:
         df_score.to_csv(go_up(1) + f'/saved_data/df_score_{os.getpid()}.csv', index=False)
         np.save(go_up(1) + f'/saved_data/beta_tensor_{os.getpid()}', beta_tensor)
@@ -133,7 +135,7 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--n_components', type=int, default=15,
                         help='Number of PCA components to keep. Valid if variable_number is False. The default is 15')
     parser.add_argument('-s', '--save_outputs', action='store_false',
-                        help='Choose whether or not to save df_score as csv and beta_tensor as .npy')
+                        help='Choose whether or not to save the outputrs. The default is True')
     args = parser.parse_args()
     levels = {'critical': logging.CRITICAL,
               'error': logging.ERROR,
@@ -148,22 +150,17 @@ if __name__ == '__main__':
 
     start = time.time()
     df_returns = pd.read_csv(go_up(1) +
-                             "/saved_data/ReturnsData.csv")
+                             "/saved_data/RusselReturnsData.csv")
     df = [df_returns[:1510], df_returns[1258:2769], df_returns[2517:4028], df_returns[3776:5287], df_returns[5035:]]
-    # # df = [df_returns[:260], df_returns[260:520], df_returns[520:780]]
-    #
     processes = [mp.Process(target=generate_score, args=(i, args.n_components)) for i in df]
 
     for p in processes:
         p.start()
-        time.sleep(0.2)
 
     for p in processes:
         p.join()
-        time.sleep(0.2)
 
-    # df_score, beta_tensor = generate_score(df_returns, n_factor=args.n_components, variable_n_factor=args.variable_number,
-    #                                        lookback_for_factors=252, lookback_for_residual=60, export=args.save_outputs)
+    # df_score, beta_tensor = generate_score(df_returns, n_factor=args.n_components, variable_n_factor=args.variable_number, lookback_for_factors=252, lookback_for_residual=60, export=args.save_outputs)
 
     # spy = pd.read_csv(go_up(1) + '/saved_data/spy.csv')
     # beta_spy = SPY_beta(df_returns, spy, export=args.save_outputs)
