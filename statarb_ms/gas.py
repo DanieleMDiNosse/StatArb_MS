7593fd366e4df1268d8c0dac9e7f8033383f8c56
DanieleMDiNosse <danielemdinosse@gmail.com>
2022-01-31 10:48:14 +0100

    test on syntethic data

--- statarb_ms/gas.py
+++ statarb_ms/gas.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import minimize, least_squares
+from scipy.optimize import minimize
 from makedir import go_up
 import matplotlib.pyplot as plt
 import time
@@ -8,55 +8,79 @@ from numba import njit, prange
 from tqdm import tqdm
 import math
 import reduced_loglikelihood
-
-def model():
-    s = np.zeros(shape=500)
-    sgm = np.array([2 + 0.5 * np.sin(np.pi * t / 100) for t in range(s.shape[0])])
-    for t in range(s.shape[0]):
-        s[t] = sgm[t] * np.random.normal()
-    return s, sgm
-
-def model_loglikelihood(params, s):
-    loglikelihood = np.zeros_like(s)
-    var = np.zeros_like(s)
-    var[0] = 0.1
-    T = s.shape[0]
-    omega, alpha, beta = params[0], params[1], params[2]
-    for i in range(T-1):
-        loglikelihood[i] = -0.5 * (np.log(2*np.pi*var[i]) + s[i]**2/var[i])
-        var[i + 1] = np.abs(omega) + np.abs(alpha) * (s[i]**2 - var[i]) + np.abs(beta) * var[i]
-    return -loglikelihood.sum()
-
-def model_estimation(fun, s, sgm):
-    init_params = np.random.uniform(0, 0.1, size=3)
-    print(f'Initial guess: \n {init_params}')
-    res = minimize(fun, init_params, s, method='BFGS', options={'gtol': 1e-5})
-    omega, alpha, beta = res.x[0], res.x[1], res.x[2]
-    print(res)
-    var = np.ones_like(s)
-    s = np.ones_like(s)
-    for i in range(1, s.shape[0]):
-        var[i] = omega + alpha * (s[i-1]**2 - var[i-1]) + beta * var[i-1]
-        s[i] = np.sqrt(var[i]) * np.random.normal()
-    plt.figure()
-    plt.scatter(np.linspace(0,s.shape[0], s.shape[0]), s, s=4, label='Filtered values')
-    plt.plot(sgm, label='Real dynamics')
-    plt.grid(True)
-    plt.tight_layout(True)
-    plt.show()
+from tqdm import tqdm
 
 
-def estimation(fun, X, n_params, method='Nelder-Mead', update='gaussian', verbose=False, visualization=False):
+def synt_gas(a, omega, alpha, beta, sgm):
+    eps = np.random.normal(0, sgm, size=1000)
+    X = np.zeros(shape=1000)
+    b = np.zeros(shape=1000)
+    b[0] = 0
+    X[0] = eps[0]
+    for t in range(1, X.shape[0] - 1):
+        # b[t + 1] = omega + alpha * X[t - 1] * eps[t] / sgm**2 + beta * b[t]
+        if (t < 200):
+            b[t + 1] = 0.2
+        if (t > 199):
+            b[t + 1] = 0.6
+        X[t + 1] = a + b[t + 1] * X[t] + eps[t + 1]
+    return X, b, eps
+
+
+def model_loglikelihood(params, X):
+    T = X.shape[0]
+    a, omega, alpha, beta, sgm = params[0], params[1], params[2], params[3], params[4]
+    b = np.zeros_like(X)
+    for i in range(1, T - 1):
+        b[i + 1] = omega + alpha * \
+            (X[i] - a - b[i] * X[i - 1]) * X[i - 1] / sgm**2 + beta * b[i]
+
+    sum = 0
+    for i in range(T - 1):
+        sum += - 0.5 * np.log(sgm**2) - 0.5 * \
+            (X[i + 1] - a - b[i + 1] * X[i])**2 / sgm**2
+
+    return -sum / T
+
+
+def model_estimation(fun, X, init_params):
+    res = minimize(fun, init_params, X, method='BFGS',
+                   options={'maxiter': 1000})
+    std_err = np.sqrt([res.hess_inv[i, i] * (1 / X.shape[0])
+                      for i in range(init_params.shape[0])])
+    estimates = res.x
+    estimates_up = res.x + std_err
+    estimates_down = res.x - std_err
+
+    X, b = np.zeros_like(X), np.zeros_like(X)
+    X_up, b_up = np.zeros_like(X), np.zeros_like(X)
+    X_down, b_down = np.zeros_like(X), np.zeros_like(X)
+
+    for t in range(1, X.shape[0] - 1):
+        X[t + 1] = estimates[0] + b[t + 1] * X[t] + eps[t + 1]
+        b[t + 1] = estimates[1] + estimates[2] * X[t - 1] * \
+            eps[t] / estimates[4]**2 + estimates[3] * b[t]
+
+        X_up[t + 1] = estimates_up[0] + b_up[t + 1] * X_up[t] + eps[t + 1]
+        b_up[t + 1] = estimates_up[1] + estimates_up[2] * X_up[t - 1] * \
+            eps[t] / estimates_up[4]**2 + estimates_up[3] * b_up[t]
+
+        X_down[t + 1] = estimates_down[0] + \
+            b_down[t + 1] * X_down[t] + eps[t + 1]
+        b_down[t + 1] = estimates_down[1] + estimates_down[2] * X_down[t -
+                                                                       1] * eps[t] / estimates_down[4]**2 + estimates_down[3] * b_down[t]
+
+    return [X, X_up, X_down], [b, b_up, b_down], res, std_err
+
+
+def estimation(fun, X, init_params, method='Nelder-Mead', update='gaussian', verbose=False, visualization=False):
     '''Estimation of GAS parameters'''
     T = X.shape[0]
     b = np.ones(shape=T)
     xi = np.zeros(shape=T)
     X_est = np.zeros_like(X)
     sigma = 1
-    if n_params == 4:
-        init_params = np.random.uniform(0, 1, size=4)
-    if n_params == 5:
-        init_params = np.random.uniform(0,1, size=5)
+    n_params = init_params.shape[0]
     res = minimize(fun, init_params, (X, n_params, update, sigma),
                    method=method)
     if verbose:
@@ -70,32 +94,34 @@ def estimation(fun, X, n_params, method='Nelder-Mead', update='gaussian', verbos
 
     for i in range(1, T - 1):
         if update == 'gaussian':
-            b[i + 1] = omega + alpha * xi[i] * X[i-1] / sigma**2 + beta * b[i]
-            xi[i + 1] = X[i + 1] - a - b[i + 1] * X[i] # prediction error decomposition
+            b[i + 1] = omega + alpha * xi[i] * \
+                X[i - 1] / sigma**2 + beta * b[i]
+            xi[i + 1] = X[i + 1] - a - b[i + 1] * \
+                X[i]  # prediction error decomposition
         if update == 'logistic':
-            b[i + 1] = omega + alpha * ((X[i] - 1 / (1 + np.exp(-b[i])) * X[i - 1]) * np.exp(-b[i]) / (1 + np.exp(-b[i]))**2 * X[i - 1] / sq_sgm) + beta * b[i]
-            xi[i + 1] = X[i + 1] - a - 1 / (1 + np.exp(-b[i + 1])) * X[i] # prediction error decomposition
-        if update == 'logarithm':
-            b[i + 1] = omega + alpha * X[i - 1] / (b[i] * sq_sgm) * (X[i] - a - np.log(b[i]) * X[i-1]) + beta * b[i]
-            xi[i + 1] = X[i + 1] - a - np.log(b[i + 1]) * X[i] # prediction error decomposition
+            b[i + 1] = omega + alpha * ((X[i] - 1 / (1 + np.exp(-b[i])) * X[i - 1]) *
+                                        np.exp(-b[i]) / (1 + np.exp(-b[i]))**2 * X[i - 1] / sq_sgm) + beta * b[i]
+            # prediction error decomposition
+            xi[i + 1] = X[i + 1] - a - 1 / (1 + np.exp(-b[i + 1])) * X[i]
 
-    if update == 'logistic': b = 1 / (1 + np.exp(-b))
-    if update == 'logarithm': b = np.log(b)
+    if update == 'logistic':
+        b = 1 / (1 + np.exp(-b))
 
     if visualization:
         X_est = np.zeros_like(X)
         b_est = np.zeros_like(b)
-        for t in range (1, X_est.shape[0] - 1):
+        for t in range(1, X_est.shape[0] - 1):
             X_est[t + 1] = a + b_est[t + 1] * X_est[t] + xi[t + 1]
-            b_est[t + 1] = omega + alpha * xi[t] * X_est[t - 1] + beta * b_est[t]
-        plt.figure(figsize=(12,8))
+            b_est[t + 1] = omega + alpha * xi[t] * \
+                X_est[t - 1] + beta * b_est[t]
+        plt.figure(figsize=(12, 8))
         plt.plot(X, label='Data')
         plt.plot(X_est, label='Filtered Data')
         plt.legend()
         plt.grid(True)
         plt.show()
 
-    return b, a, xi
+    return b, a, xi, res
 
 
 if __name__ == '__main__':
@@ -103,10 +129,13 @@ if __name__ == '__main__':
         description='Generator of historical price data')
     parser.add_argument("-l", "--log", default="info",
                         help=("Provide logging level. Example --log debug', default='info"))
-    parser.add_argument("-u", "--update", type=int, default=0, help="Update scheme for gas estimation")
-    parser.add_argument("-n", "--n_params", type=int, default=4, help="Number of parameters to be estimates")
+    parser.add_argument("-u", "--update", type=int, default=0,
+                        help="Update scheme for gas estimation")
+    parser.add_argument("-n", "--n_params", type=int, default=4,
+                        help="Number of parameters to be estimates")
     parser.add_argument("-v", "--verbose", action='store_true')
     parser.add_argument("-vv", "--visualization", action='store_true')
+    parser.add_argument("-c", "--convergence", action='store_true')
     parser.add_argument("-m", "--model", action='store_true')
 
     args = parser.parse_args()
@@ -117,12 +146,145 @@ if __name__ == '__main__':
         update = 'gaussian'
     if args.update == 1:
         update = 'logistic'
-    if args.update == 2:
-        update = 'logarithm'
 
     if args.model:
-        s, sgm = model()
-        model_estimation(model_loglikelihood, s, sgm)
+        omega = 0.05
+        alpha = 0.08
+        beta = 0.05
+        sgm = 0.1
+        a = 0.02
+        n = 560
+        init_params = np.random.uniform(0, 0.1, size=5)
+        X, b, eps = synt_gas(a, omega, alpha, beta, sgm)
+        fig, axs = plt.subplots(1, 2, tight_layout=True, figsize=(14, 5))
+        axs[0].plot(X[:n], 'k', label='Real', linewidth=1)
+        axs[1].plot(b[:n], 'b', label='Real', linewidth=1)
+
+        XX, B, res, std_err = model_estimation(
+            model_loglikelihood, X, init_params)
+
+        print('True values: ', [a, omega, alpha, beta, sgm])
+        print('Estimated values: ', res.x)
+        print('Standard errors: ', std_err)
+
+        axs[0].plot(XX[0][:n], 'green', label='Filtered', linewidth=1)
+        axs[0].fill_between(list(range(n)), XX[2][:n], XX[1]
+                            [:n], color='green', alpha=0.3)
+
+        axs[1].plot(B[0][:n], 'crimson', label='Filtered', linewidth=1)
+        axs[1].fill_between(list(range(n)), B[2][:n], B[1]
+                            [:n], color='crimson', alpha=0.3)
+
+        axs[0].legend()
+        axs[1].legend()
+        plt.show()
+
+        if args.convergence:
+            N = 400
+            est_par = np.empty(shape=(N, 4))
+            par = np.random.uniform(0, 1, size=(N, 4))
+            fun_val = []
+            b_val = []
+            for i in tqdm(range(len(par))):
+                XX, B, res, std_err = model_estimation(
+                    model_loglikelihood, X, init_params)
+                fun_val.append(res.fun)
+                est_par[i] = res.x
+                b_val.append(B[0][-1])
+            plt.figure(figsize=(12, 8), tight_layout=True)
+            ax0 = plt.subplot(2, 3, 4)
+            ax0.plot(b_val, 'k', linewidth=1)
+            ax0.title.set_text('b(60) values')
+            ax1 = plt.subplot(2, 3, 1)
+            ax1.plot(fun_val, 'k', linewidth=1)
+            ax1.title.set_text('Likelihood Evaluations')
+            ax2 = plt.subplot(args.n_params, 3, 2)
+            ax2.plot(par[:, 0], linewidth=1)
+            ax2.title.set_text('Initial Omega')
+            ax3 = plt.subplot(args.n_params, 3, 5)
+            ax3.plot(par[:, 1], linewidth=1)
+            ax3.title.set_text('Initial a')
+            ax4 = plt.subplot(args.n_params, 3, 8)
+            ax4.plot(par[:, 2], linewidth=1)
+            ax4.title.set_text('Initial alpha')
+            ax5 = plt.subplot(args.n_params, 3, 11)
+            ax5.plot(par[:, 3], linewidth=1)
+            ax5.title.set_text('Initial beta')
+
+            ax6 = plt.subplot(args.n_params, 3, 3)
+            ax6.plot(est_par[:, 0], 'k', linewidth=1)
+            ax6.title.set_text('Estimated Omega')
+            ax7 = plt.subplot(args.n_params, 3, 6)
+            ax7.plot(est_par[:, 1], 'k', linewidth=1)
+            ax7.title.set_text('Estimated a')
+            ax8 = plt.subplot(args.n_params, 3, 9)
+            ax8.plot(est_par[:, 2], 'k', linewidth=1)
+            ax8.title.set_text('Estimated alpha')
+            ax9 = plt.subplot(args.n_params, 3, 12)
+            ax9.plot(est_par[:, 3], 'k', linewidth=1)
+            ax9.title.set_text('Estimated beta')
+            plt.grid(True)
+            plt.show()
+        exit()
+
+    if args.convergence:
+        X = np.load(go_up(1) + '/saved_data/dis_res.npy')
+        days = X.shape[0]
+        n_stocks = X.shape[1]
+        time_list = []
+        for day in range(100):
+            start = time.time()
+            for stock in range(n_stocks):
+                x = X[day, stock, :]
+                fun_val = []
+                b_val = []
+                est_par = np.empty(shape=(100, args.n_params))
+                par = np.random.uniform(0, 1, size=(100, args.n_params))
+                for i in range(len(par)):
+                    b, a, xi, res = estimation(
+                        reduced_loglikelihood.reduced_loglikelihood, x, par[i], update=update, verbose=args.verbose, visualization=args.visualization)
+                    fun_val.append(res.fun)
+                    est_par[i] = res.x
+                    b_val.append(b[-1])
+                    # add the difference between the initial guess and the estimated parameters
+                plt.figure(figsize=(12, 8), tight_layout=True)
+                ax0 = plt.subplot(2, 3, 4)
+                ax0.plot(b_val, 'k')
+                ax0.title.set_text('b(60) values')
+                ax1 = plt.subplot(2, 3, 1)
+                ax1.plot(fun_val, 'k')
+                ax1.title.set_text('Likelihood Evaluations')
+                ax2 = plt.subplot(args.n_params, 3, 2)
+                ax2.plot(par[:, 0])
+                ax2.title.set_text('Initial Omega')
+                ax3 = plt.subplot(args.n_params, 3, 5)
+                ax3.plot(par[:, 1])
+                ax3.title.set_text('Initial a')
+                ax4 = plt.subplot(args.n_params, 3, 8)
+                ax4.plot(par[:, 2])
+                ax4.title.set_text('Initial alpha')
+                ax5 = plt.subplot(args.n_params, 3, 11)
+                ax5.plot(par[:, 3])
+                ax5.title.set_text('Initial beta')
+
+                ax6 = plt.subplot(args.n_params, 3, 3)
+                ax6.plot(est_par[:, 0])
+                ax6.title.set_text('Estimated Omega')
+                ax7 = plt.subplot(args.n_params, 3, 6)
+                ax7.plot(est_par[:, 1])
+                ax7.title.set_text('Estimated a')
+                ax8 = plt.subplot(args.n_params, 3, 9)
+                ax8.plot(est_par[:, 2])
+                ax8.title.set_text('Estimated alpha')
+                ax9 = plt.subplot(args.n_params, 3, 12)
+                ax9.plot(est_par[:, 3])
+                ax9.title.set_text('Estimated beta')
+                plt.grid(True)
+                plt.show()
+                if (math.isnan(b[-1])) or (b[-1] < 0):
+                    print(b[-1])
+            end = time.time()
+            print(f'time day {day}: ', end - start)
 
     else:
         X = np.load(go_up(1) + '/saved_data/dis_res.npy')
@@ -133,13 +295,13 @@ if __name__ == '__main__':
             start = time.time()
             for stock in range(n_stocks):
                 x = X[day, stock, :]
-                b, a, xi = estimation(
+                init_params = np.random.uniform(0, 1, size=args.n_params)
+                b, a, xi, fun = estimation(
                     reduced_loglikelihood.reduced_loglikelihood, x, n_params=args.n_params, update=update, verbose=args.verbose, visualization=args.visualization)
                 if (math.isnan(b[-1])) or (b[-1] < 0):
                     print(b[-1])
             end = time.time()
-            print(f'time day {day}: ', end-start)
-
+            print(f'time day {day}: ', end - start)
 
 #     fig = plt.figure()
 #     ax = plt.gca(projection='3d')
---
+++ statarb_ms/gas_prova.py
@@ -0,0 +1,94 @@
+from scipy.optimize import minimize
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import poisson
+from tqdm import tqdm
+import time
+
+
+def gas_poisson_generate(w, A, B, size):
+    y = np.zeros(shape=size)
+    f = np.zeros(shape=size)
+    s = np.zeros(shape=size)
+    ###############################################
+    # Initial conditions for the recursion:
+    ###############################################
+    f[0] = 0
+    y[0] = np.random.poisson(f[0])
+    ###############################################
+    # GAS mechanism:
+    ###############################################
+    for t in range(size - 1):
+        # if t < 300: f[t + 1] = 0
+        # if t > 300: f[t + 1] = 0.5
+        # if t > 600: f[t + 1] = 0
+        f[t + 1] = 0.5 * np.sin(np.pi * t / 100)
+        # f[t+1] = w + A * (y[t]-np.exp(f[t]))*(np.exp(f[t])) + B * f[t]
+        y[t + 1] = np.random.poisson(np.exp(f[t + 1]))
+
+    return y, f
+
+
+def gas_poisson(q, y):
+    T = y.shape[0]
+    A, B, w = q[0], q[1], q[2]
+    f = np.zeros_like(y)
+    for t in range(T - 1):
+        f[t + 1] = w + A * (y[t] - np.exp(f[t])) * (np.exp(f[t])) + B * f[t]
+
+    sum = 0
+    for t in range(T):
+        sum = sum + poisson.logpmf(y[t], mu=np.exp(f[t]))
+
+    return - sum / y.shape[0]
+
+
+if __name__ == '__main__':
+    plt.style.use('seaborn')
+    np.random.seed(666)
+
+    A = 0.081
+    B = -0.395
+    w = 0.183
+    estimates = np.zeros(shape=3)
+    n = 1000
+    # Generate Data
+    y, f = gas_poisson_generate(w, A, B, size=1000)
+    fig, axs = plt.subplots(2, 1, tight_layout=True, figsize=(12, 5))
+    axs[0].plot(y, linewidth=1)
+    axs[0].set_ylabel('Y')
+    axs[1].plot(f[:n], 'k', linewidth=1, label='real')
+    axs[1].set_ylabel('f')
+
+    # Fit a GAS model on the Data
+    init_params = np.random.uniform(0, 0.1, size=3)
+    res = minimize(gas_poisson, init_params, y, method='BFGS')
+    std_err = np.sqrt([res.hess_inv[i, i] * (1 / y.shape[0])
+                      for i in range(init_params.shape[0])])
+    par_est = res.x
+    par_est_up = res.x + std_err
+    par_est_down = res.x - std_err
+
+    print('True values: ', [A, B, w])
+    print('Estimated values: ', par_est)
+    print('Standard errors: ', std_err)
+
+    # Filtered Data
+    f_est = np.zeros_like(y)
+    f_est_up = np.zeros_like(y)
+    f_est_down = np.zeros_like(y)
+
+    for t in range(f_est.shape[0] - 1):
+        f_est[t + 1] = par_est[2] + par_est[0] * \
+            (y[t] - np.exp(f_est[t])) * \
+            (np.exp(f_est[t])) + par_est[1] * f_est[t]
+        f_est_up[t + 1] = par_est_up[2] + par_est_up[0] * \
+            (y[t] - np.exp(f_est_up[t])) * \
+            (np.exp(f_est_up[t])) + par_est_up[1] * f_est_up[t]
+        f_est_down[t + 1] = par_est_down[2] + par_est_down[0] * (y[t] - np.exp(
+            f_est_down[t])) * (np.exp(f_est_down[t])) + par_est_down[1] * f_est_down[t]
+    axs[1].plot(f_est[:n], 'crimson', linewidth=1, label='filtered')
+    axs[1].fill_between(list(range(n)), f_est_down[:n],
+                        f_est_up[:n], color='crimson', alpha=0.4)
+    axs[1].legend()
+    plt.show()
